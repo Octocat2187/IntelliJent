296748c4b6d747d99cb77868600ee188bba1e538
Octocat <ArmourIT21@gcc.edu>
2026-02-25 08:50:46 -0500

    Implemented filter logic

--- backend/src/main/java/edu/gcc/intellijent/CourseCodeFilter.java
+++ backend/src/main/java/edu/gcc/intellijent/CourseCodeFilter.java
@@ -1,14 +1,15 @@
 package edu.gcc.intellijent;
+
 import java.util.ArrayList;
 
-public class ProfFilter extends Filter {
-    public String prof;
+public class CourseCodeFilter extends Filter{
+    public String courseCode;
 
     @Override
     public ArrayList<Course> ApplyFilter(ArrayList<Course> searchResults) {
         ArrayList<Course> filteredResults = new ArrayList<>();
         for (Course course : searchResults) {
-            if(course.GetProf().equals(prof)){
+            if(course.GetCode().equals(courseCode)){
                 filteredResults.add(course);
             }
         }
--- backend/src/main/java/edu/gcc/intellijent/DateTimeFilter.java
+++ backend/src/main/java/edu/gcc/intellijent/DateTimeFilter.java
@@ -1,13 +1,21 @@
 package edu.gcc.intellijent;
+import java.sql.Time;
 import java.util.ArrayList;
 
 public class DateTimeFilter extends Filter {
-    String beginTime;
-    String endTime;
+    public Time beginTime;
+    public Time endTime;
     String days;
 
     @Override
     public ArrayList<Course> ApplyFilter(ArrayList<Course> searchResults) {
-        return null;
+        ArrayList<Course> filteredResults = new ArrayList<>();
+        for (Course course : searchResults) {
+            if(course.GetStartTime().equals(beginTime) && course.GetEndTime().equals(endTime) && course.GetDays().equals(days)){
+                filteredResults.add(course);
+            }
+        }
+
+        return filteredResults;
     }
 }
