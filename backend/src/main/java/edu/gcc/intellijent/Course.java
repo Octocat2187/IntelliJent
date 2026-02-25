464aa6de821b4f71fa4c694365bd0309ed2c6988
Octocat <ArmourIT21@gcc.edu>
2026-02-25 08:22:43 -0500

    Added getters and setters for member vars

--- backend/src/main/java/edu/gcc/intellijent/Course.java
+++ backend/src/main/java/edu/gcc/intellijent/Course.java
@@ -9,6 +9,63 @@ public class Course {
     private int numCredits;
     private boolean full;
 
+    //Getters and setters
+    public int getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(int courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(int courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public int getNumCredits() {
+        return numCredits;
+    }
+
+    public void setNumCredits(int numCredits) {
+        this.numCredits = numCredits;
+    }
+
+    public boolean isFull() {
+        return full;
+    }
+
+    public void setFull(boolean full) {
+        this.full = full;
+    }
+
     public boolean isAvailable(){
         //Returns true if the course is not full, false otherwise
         return true;
@@ -16,5 +73,6 @@ public class Course {
 
     public void viewCourseInfo(){
         //Returns all the course information
+
     }
 }
