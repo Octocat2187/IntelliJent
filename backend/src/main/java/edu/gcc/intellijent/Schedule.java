241c4e79f2cab36e285cd2f2ef09ed36cc83820b
SCHROEDERJM22 <jacob.schroeder57@gmail.com>
2026-03-11 14:42:26 -0400

    Revert "isCourseSchedulable is no longer broken"
    This reverts commit 4598f9e4ae8a9584df4b51234011888f9d1fb60f.

--- backend/src/main/java/edu/gcc/intellijent/Schedule.java
+++ backend/src/main/java/edu/gcc/intellijent/Schedule.java
@@ -32,35 +32,15 @@ public class Schedule {
      * @param course the to be Added course checked for scheduling conflicts
      * @return true if the course can be scheduled (no overlap), false otherwise
      */
-    public boolean isCourseSchedulable(Course course){
-        for (int i = 0; i < Schedule.size(); i++) {
-           Course schCor = Schedule.get(i);
-            if (course.getStartTime().compareTo(schCor.getEndTime()) < 0 &&
-                    course.getEndTime().compareTo(schCor.getStartTime()) > 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
-    /**
-     * Returns a list of alternative courses from the search results that do not have scheduling conflicts with the given conflicting course.
-     *
-     * @param searchResults the list of courses to search through for alternatives
-     * might be switched to data base query instead of searching through search results, but this is easier for now
-     * @param conflictingCourse the course that has a scheduling conflict
-     * @return an ArrayList of alternative courses that can be scheduled without conflicts
-     */
-    public ArrayList<Course> AlternativeCourses(ArrayList<Course> searchResults, Course conflictingCourse){
-        ArrayList<Course> alternatives = new ArrayList<Course>();
-        for (Course course : searchResults) {
-            if (isCourseSchedulable(course) && course.getCourseCode() == conflictingCourse.getCourseCode()){
-                alternatives.add(course);
-            }
-        }
-        return alternatives;
-
-    }
+//    public boolean isCourseSchedulable(Course course){
+//        for (int i = 0; i < Schedule.size(); i++) {
+//           Course schCor = Schedule.get(i);
+//            if (course.getStartTime() < schCor.getEndTime() &&
+//                    course.getEndTime() > schCor.getStartTime()) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 
 }
