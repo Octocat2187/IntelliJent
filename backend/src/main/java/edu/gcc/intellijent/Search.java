e6682ae284ed218fd9d4ebcd614c9a8f30b59089
Octocat <ArmourIT21@gcc.edu>
2026-03-13 08:29:48 -0400

    Search logic created and filter logic created

--- backend/src/main/java/edu/gcc/intellijent/Search.java
+++ backend/src/main/java/edu/gcc/intellijent/Search.java
@@ -8,15 +8,16 @@ public class Search {
     private Filter currFilter;
     private String searchQuery;
 
-    ArrayList<Filter> filterList;
+
+    CourseCatalog courseCatalog;
 
     public Search(String query) {
         searchQuery = query;
     }
 
-    public Search(String query, ArrayList<Filter> filterList) {
+    public Search(CourseCatalog catalog, String query) {
+        this.courseCatalog = catalog;
         searchQuery = query;
-        this.filterList = filterList;
 
         CourseSearch();
     }
@@ -24,6 +25,46 @@ public class Search {
     private void CourseSearch() {
         resultList = new ArrayList<Course>();
 
+        for (Course c : courseCatalog.getClasses()){
+            boolean isMatch = false;
+
+            if(c.getName().toLowerCase().contains(searchQuery.toLowerCase())){
+                isMatch = true;
+            }
+
+            for(String faculty : c.getFaculty()){
+                if(faculty.toLowerCase().contains(searchQuery.toLowerCase())){
+                    isMatch = true;
+                }
+            }
+
+            if(c.getSubject().toLowerCase().contains(searchQuery.toLowerCase())){
+                isMatch = true;
+            }
+
+            if(c.getCourseCode().toLowerCase().contains(searchQuery.toLowerCase())){
+                isMatch = true;
+            }
+
+            if(Integer.toString(c.getNumber()).contains(searchQuery.toLowerCase())){
+                isMatch = true;
+            }
+
+            if(isMatch){
+                resultList.add(c);
+            }
+        }
+
+
 
     }
+
+    public ArrayList<Course> FilterResults(ArrayList<Filter> filterList) {
+        ArrayList<Course> filteredResults = new ArrayList<>();
+        for (Filter filter : filterList) {
+            filteredResults = filter.ApplyFilter(resultList);
+        }
+        return filteredResults;
+    }
+
 }
