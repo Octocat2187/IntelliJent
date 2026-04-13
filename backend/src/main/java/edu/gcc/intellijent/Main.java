1b8ee95e78818466ca40424e52554ba3bbabd54b
Mizmazer <joshburick@gmail.com>
2026-04-13 01:42:44 -0400

    Added required courses for major button

--- backend/src/main/java/edu/gcc/intellijent/Main.java
+++ backend/src/main/java/edu/gcc/intellijent/Main.java
@@ -5,7 +5,6 @@ import io.javalin.Javalin;
 
 import java.io.InputStream;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
@@ -47,73 +46,14 @@ public class Main {
             InputStream majorInput = Main.class.getClassLoader().getResourceAsStream("majors.json");
 
             MajorCatalog majorCatalog = mapper.readValue(majorInput, MajorCatalog.class);
-/*
-            Scanner scanner = new Scanner(System.in);
-
-            // Create account
             Account account = new Account("student", "password");
+            MajorController majorController = new MajorController(majorCatalog, account);
+            majorController.registerRoutes(app);
 
-            // Select Major
-            System.out.println("\nAvailable Majors:");
-
-            for (Major m : majorCatalog.getMajors()) {
-                System.out.println("- " + m.getName());
-            }
-
-            System.out.print("\nEnter your major: ");
-            String selectedMajor = scanner.nextLine();
-
-            for (Major m : majorCatalog.getMajors()) {
-                if (m.getName().equalsIgnoreCase(selectedMajor)) {
-                    account.setMajor(m);
-                    break;
-                }
-            }
-
-            // Display major requirements
-            System.out.println("\nMajor Requirements:");
-            account.viewMajorRequirements();
-
-            // Course search
-            System.out.print("\nEnter course code to search (ex: COMP141): ");
-            String userInput = scanner.nextLine().toUpperCase();
-
-            boolean found = false;
-
-            for (Course course : courses) {
-
-                String courseCode = course.getSubject() + course.getNumber();
-
-                if (courseCode.equals(userInput)) {
-
-                    System.out.println("\nCourse Name: " + course.getName());
-                    System.out.println("Section: " + course.getSection());
-                    System.out.println("Location: " + course.getLocation());
-                    System.out.println("Professor: " + course.getFaculty());
-                    System.out.println("Credits: " + course.getCredits());
-
-                    if (course.getTimes() != null) {
-                        for (ClassTime time : course.getTimes()) {
-                            System.out.println(
-                                    "Time: " +
-                                            time.getDay() + " " +
-                                            time.getStart_time() + " - " +
-                                            time.getEnd_time()
-                            );
-                        }
-                    }
-
-                    found = true;
-                }
-            }
-
-            if (!found) {
-                System.out.println("Course not found.");
-            }
-*/
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
 
