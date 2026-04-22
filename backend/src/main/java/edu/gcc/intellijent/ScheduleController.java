f0842f44c1c594e58348a3b1c6e573ee945880ce
Octocat <ArmourIT21@gcc.edu>
2026-04-22 08:50:02 -0400

    Added roulette path to ScheduleController.java

--- backend/src/main/java/edu/gcc/intellijent/ScheduleController.java
+++ backend/src/main/java/edu/gcc/intellijent/ScheduleController.java
@@ -23,6 +23,23 @@ public class ScheduleController {
             ctx.json(schedule.Schedule);
         });
 
+        app.get("/roulette", ctx ->{
+            String username =  ctx.queryParam("username");
+            int guess = Integer.parseInt(ctx.queryParam("guess"));
+
+            Schedule schedule = userScheduleStore.getSchedule(username);
+
+            if(!schedule.wonRoulette(guess)){
+                // you lose
+                // delete schedule
+                schedule.clearSchedule();
+
+                ctx.status(204);
+            }
+
+            ctx.status(200);
+        });
+
         app.post("/schedule", ctx -> {
             String username = ctx.queryParam("username");
 
