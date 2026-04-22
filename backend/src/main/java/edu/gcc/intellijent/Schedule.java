392f7a7a0feb3edc478a5cd2b59f6d0844c1b445
Octocat <ArmourIT21@gcc.edu>
2026-04-22 08:33:03 -0400

    Initial commit, added wonRoulette function to Schedule.java

--- backend/src/main/java/edu/gcc/intellijent/Schedule.java
+++ backend/src/main/java/edu/gcc/intellijent/Schedule.java
@@ -115,6 +115,12 @@ public class Schedule {
         return alternativeCourses;
     }
 
+    public boolean wonRoulette(int guess){
+        int winningNumber = (int)(Math.random() * 50) + 1;
+
+        return guess == winningNumber;
+    }
+
     public boolean isCourseAdded(){
         return CourseAdded;
     }
